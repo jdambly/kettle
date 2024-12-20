09c509c82db19646f69968cc62e4e71be8205b4a
jdambly <jdambly@netskope.com>
2024-12-20 14:28:10 -0800

    updated/fixed unit tests using NamespaceName

--- api/v1alpha1/network_types_test.go
+++ api/v1alpha1/network_types_test.go
@@ -4,8 +4,9 @@ import (
 	"github.com/jdambly/kettle/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 var _ = Describe("Network GetIPs", func() {
@@ -92,12 +93,24 @@ var _ = Describe("Network GetIPs", func() {
 		BeforeEach(func() {
 			// todo this needs to updated now that the assigned ips are a map
 			network.Status.AssignedIPs = v1alpha1.AllocatedIPMap{
-				"1234": {IP: "10.1.0.2", PodName: "test-pod1"},
-				"1235": {IP: "10.1.0.3", PodName: "test-pod2"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod1",
+				}: {IP: "10.1.0.2", PodName: "test-pod1"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod2",
+				}: {IP: "10.1.0.3", PodName: "test-pod2"},
 			}
 			newNetwork.Status.AssignedIPs = v1alpha1.AllocatedIPMap{
-				"1234": {IP: "10.1.0.2", PodName: "test-pod1"},
-				"1235": {IP: "10.1.0.3", PodName: "test-pod2"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod1",
+				}: {IP: "10.1.0.2", PodName: "test-pod1"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod2",
+				}: {IP: "10.1.0.3", PodName: "test-pod2"},
 			}
 			allocatableIPs, err := network.GetIPs()
 			newAllocatableIPs, newErr := newNetwork.GetIPs()
@@ -114,88 +127,88 @@ var _ = Describe("Network GetIPs", func() {
 		})
 		It("Returns true when the status are not Equal", func() {
 			newNetwork.Status.AssignedIPs = v1alpha1.AllocatedIPMap{
-				"123456": {IP: "10.1.0.2", PodName: "test-pod3"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod3",
+				}: {IP: "10.1.0.2", PodName: "test-pod3"},
 			}
 			Expect(network.ShouldReconcile(newNetwork)).To(BeTrue())
 		})
 		It("Return true when there is duplicate ips", func() {
 			newNetwork.Status.AssignedIPs = v1alpha1.AllocatedIPMap{
-				"123456": {IP: "10.1.0.2", PodName: "test-pod3"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod3",
+				}: {IP: "10.1.0.2", PodName: "test-pod3"},
 			}
 			Expect(network.ShouldReconcile(newNetwork)).To(BeTrue())
 		})
 	})
 	Context("When allocating an IP to a pod", func() {
-		var pod *corev1.Pod
 
+		var req ctrl.Request
 		BeforeEach(func() {
-			pod = &corev1.Pod{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-pod",
+			network.Status.FreeIPs = []string{"10.0.0.2", "10.0.0.3"}
+
+			req = ctrl.Request{
+				NamespacedName: types.NamespacedName{
 					Namespace: "default",
-					UID:       "1234",
+					Name:      "test-pod",
 				},
 			}
-			network.Status.FreeIPs = []string{"10.0.0.2", "10.0.0.3"}
 		})
 
 		It("should allocate the first free IP to the pod", func() {
-			_, err := network.Allocate(pod)
+			_, err := network.Allocate(req)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(network.Status.FreeIPs).ToNot(ContainElement("10.0.0.2"))
-			Expect(network.Status.AssignedIPs).To(ContainElement(v1alpha1.AllocatedIP{
-				IP:        "10.0.0.2",
-				PodName:   "test-pod",
-				Namespace: "default",
-				PodUID:    "1234",
-			}))
+			Expect(network.Status.AssignedIPs).To(HaveKeyWithValue(
+				types.NamespacedName{Namespace: "default", Name: "test-pod"},
+				v1alpha1.AllocatedIP{IP: "10.0.0.2", PodName: "test-pod", Namespace: "default"},
+			))
 		})
 
 		It("should return an error if no free IPs are available", func() {
 			network.Status.FreeIPs = []string{}
-			_, err := network.Allocate(pod)
+			_, err := network.Allocate(req)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal(v1alpha1.ErrorNoIPsAvailable.Error()))
 		})
 		It("should return an error if is the ip is already allocated", func() {
 			network.Status.AssignedIPs = v1alpha1.AllocatedIPMap{
-				"1234": {IP: "10.0.0.2", PodName: "test-pod"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod",
+				}: {IP: "10.0.0.2", PodName: "test-pod"},
 			}
-			_, err := network.Allocate(pod)
+			_, err := network.Allocate(req)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal(v1alpha1.ErrIPAlreadyAllocated.Error()))
 		})
 	})
 	Context("When deallocating an IP from a pod", func() {
-		var pod *corev1.Pod
-
+		var req ctrl.Request
 		BeforeEach(func() {
-			pod = &corev1.Pod{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-pod",
+			req = ctrl.Request{
+				NamespacedName: types.NamespacedName{
 					Namespace: "default",
-					UID:       "1234",
+					Name:      "test-pod",
 				},
 			}
 			network.Status.AssignedIPs = v1alpha1.AllocatedIPMap{
-				"1234": {IP: "10.0.0.2", PodName: "test-pod"},
+				types.NamespacedName{
+					Namespace: "default",
+					Name:      "test-pod",
+				}: {IP: "10.0.0.2", PodName: "test-pod"},
 			}
 			network.Status.FreeIPs = []string{}
 		})
 
 		It("should deallocate the IP from the pod and add it back to the free IPs list", func() {
-			err := network.Deallocate(pod)
+			err := network.Deallocate(req)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(network.Status.AssignedIPs).To(BeEmpty())
 			Expect(network.Status.FreeIPs).To(ContainElement("10.0.0.2"))
-		})
-
-		It("should do nothing if the pod does not have an assigned IP", func() {
-			pod.UID = "5678"
-			err := network.Deallocate(pod)
-			Expect(err).To(HaveOccurred())
-			Expect(network.Status.AssignedIPs).To(HaveLen(1))
-			Expect(network.Status.FreeIPs).To(BeEmpty())
 		})
 	})
 })
